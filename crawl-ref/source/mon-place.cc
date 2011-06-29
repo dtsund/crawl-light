9efe3f31308456515a2ebacd32464f3a7d2f236b
Adam Borowski <kilobyte@angband.pl>
2011-06-29 16:57:56 +0200

    Start tornado with a small radius and let it wind up.
    The actual radius formula needs to be tweaked.

--- crawl-ref/source/mon-place.cc
+++ crawl-ref/source/mon-place.cc
@@ -1665,7 +1665,10 @@ static int _place_monster_aux(const mgen_data &mg,
     }
 
     if (mg.cls == MONS_TWISTER)
+    {
+        mon->props["tornado_since"].get_int() = you.elapsed_time;
         mon->add_ench(ENCH_PERM_TORNADO);
+    }
 
     if (mons_is_feat_mimic(mg.cls))
     {
