0cdb728f7daf495f1ac09df141ba74f2bcff695c
Derrick Sund <dtsund@email.arizona.edu>
2011-09-21 19:05:54 -0400

    IMPORT: Restore stat gain at level 4 for draconians which was accidentally lost.

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -3127,7 +3127,7 @@ void level_change(bool skip_attribute_increase)
                     you.redraw_armour_class = true;
                 }
 
-                if (!(you.experience_level % 4) && you.experience_level > 7)
+                if (!(you.experience_level % 4))
                 {
                     modify_stat(STAT_RANDOM, 1, false, "level gain");
                 }
