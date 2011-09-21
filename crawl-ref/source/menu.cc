e0707c753cc6ad0ff9594ecbf2b6b01b887c5d33
Derrick Sund <dtsund@email.arizona.edu>
2011-09-21 13:23:04 -0400

    IMPORT: Fix help screens not taking commend when easy_exit_menu is false.

--- crawl-ref/source/menu.cc
+++ crawl-ref/source/menu.cc
@@ -1815,6 +1815,7 @@ bool formatted_scroller::process_key(int keyin)
         break;
     }
     default:
+        moved = false;
         if (is_set(MF_SINGLESELECT))
         {
             select_items(keyin);
@@ -1830,7 +1831,7 @@ bool formatted_scroller::process_key(int keyin)
 
     if (repaint)
         draw_menu();
-    else if (moved && is_set(MF_EASY_EXIT))
+    else if (!moved || is_set(MF_EASY_EXIT))
         return (false);
 
     return (true);
