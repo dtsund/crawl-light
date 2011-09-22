crawl/crawl

Rename "beast" to "hell beast".
We need monsters named "monster", "insect", "mammal", or "creature".
Unlike, say, "human" which is about as specific as "orc" or "yaktaur", or
"dragon" which refers to the ordinary species (like "bat" vs "fire bat"),
the name "beast" doesn't say anything about the monster.

The new name might be not the best choice, if you have a better idea, please
say so.

crawl-ref/source/mon-data.h
@@ -4832,10 +4832,10 @@ static monsterentry mondata[] = {
 // reaper etc. ('2')
 
 {   // with randomised stats
-    MONS_BEAST, '2', BROWN, "beast",
+    MONS_HELL_BEAST, '2', BROWN, "hell beast",
     M_FIGHTER,
     MR_NO_FLAGS,
-    0, 10, MONS_BEAST, MONS_BEAST, MH_DEMONIC, -3,
+    0, 10, MONS_HELL_BEAST, MONS_HELL_BEAST, MH_DEMONIC, -3,
     { {AT_BITE, AF_PLAIN, 28}, {AT_TRAMPLE, AF_PLAIN, 20}, AT_NO_ATK,
        AT_NO_ATK },
     { 7, 9, 6, 0 },

dtsund/crawl-light

crawl-ref/source/mon-data.h
@@ -4810,10 +4810,10 @@ static monsterentry mondata[] = {
 // reaper etc. ('2')
 
 {   // with randomised stats
-    MONS_BEAST, '2', BROWN, "beast",
+    MONS_HELL_BEAST, '2', BROWN, "hell beast",
     M_FIGHTER,
     MR_NO_FLAGS,
-    0, 10, MONS_BEAST, MONS_BEAST, MH_DEMONIC, -3,
+    0, 10, MONS_HELL_BEAST, MONS_BEAST, MH_DEMONIC, -3,
     { {AT_BITE, AF_PLAIN, 28}, {AT_TRAMPLE, AF_PLAIN, 20}, AT_NO_ATK, AT_NO_ATK },
     { 7, 9, 6, 0 },
     0, 0, MST_NO_SPELLS, CE_NOCORPSE, Z_NOZOMBIE, S_RANDOM,