55ed143c6bfce980d59fe802e99a262b5ce65f65
Derrick Sund <dtsund@email.arizona.edu>
2011-09-23 22:44:21 -0400

    Removed the need to manually run gen_ver.sh

--- crawl-ref/source/util/gen_ver.sh
+++ crawl-ref/source/util/gen_ver.sh
@@ -10,10 +10,16 @@ else
     FINAL="$1"
 fi
 
+# Check to see if this is a cloned repo.  If not, *do* *not* *touch* build.h.
+if [ -d ../../.git ]
+then
+echo "Generating build.h..."
 # VERSION will be the short version number, TIP will be the version number with
 # a hash code.
 VERSION=`git describe --tags --always`
 TIP=`git describe --tags --always --long`
 
 printf "#define CRAWL_VERSION_FINAL %s\n#define CRAWL_VERSION_SHORT \"%s\"\n#define CRAWL_VERSION_LONG \"%s\"\n" "$FINAL" "$VERSION" "$TIP" > build.h
-
+else
+echo "Not generating build.h, this isn't a cloned repository..."
+fi
