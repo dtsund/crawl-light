crawl/crawl

Increase base HP by +3.

crawl-ref/source/ng-setup.cc
@@ -150,48 +150,48 @@ static void _jobs_stat_init(job_type which_job)
     // Note: Wanderers are correct, they've got a challenging background. - bwr
     switch (which_job)
     {
-    case JOB_FIGHTER:           s =  8; i =  0; d =  4; hp = 15; mp = 0; break;
-    case JOB_BERSERKER:         s =  9; i = -1; d =  4; hp = 15; mp = 0; break;
-    case JOB_GLADIATOR:         s =  7; i =  0; d =  5; hp = 14; mp = 0; break;
+    case JOB_FIGHTER:           s =  8; i =  0; d =  4; hp = 18; mp = 0; break;
+    case JOB_BERSERKER:         s =  9; i = -1; d =  4; hp = 18; mp = 0; break;
+    case JOB_GLADIATOR:         s =  7; i =  0; d =  5; hp = 17; mp = 0; break;
 
-    case JOB_SKALD:             s =  4; i =  4; d =  4; hp = 13; mp = 1; break;
-    case JOB_CHAOS_KNIGHT:      s =  4; i =  4; d =  4; hp = 13; mp = 1; break;
-    case JOB_DEATH_KNIGHT:      s =  5; i =  3; d =  4; hp = 13; mp = 2; break;
-    case JOB_ABYSSAL_KNIGHT:    s =  4; i =  4; d =  4; hp = 13; mp = 1; break;
+    case JOB_SKALD:             s =  4; i =  4; d =  4; hp = 16; mp = 1; break;
+    case JOB_CHAOS_KNIGHT:      s =  4; i =  4; d =  4; hp = 16; mp = 1; break;
+    case JOB_DEATH_KNIGHT:      s =  5; i =  3; d =  4; hp = 16; mp = 2; break;
+    case JOB_ABYSSAL_KNIGHT:    s =  4; i =  4; d =  4; hp = 16; mp = 1; break;
 
-    case JOB_HEALER:            s =  5; i =  5; d =  2; hp = 13; mp = 2; break;
-    case JOB_PRIEST:            s =  5; i =  4; d =  3; hp = 13; mp = 2; break;
+    case JOB_HEALER:            s =  5; i =  5; d =  2; hp = 16; mp = 2; break;
+    case JOB_PRIEST:            s =  5; i =  4; d =  3; hp = 16; mp = 2; break;
 
-    case JOB_ASSASSIN:          s =  3; i =  3; d =  6; hp = 12; mp = 0; break;
-    case JOB_STALKER:           s =  2; i =  4; d =  6; hp = 12; mp = 1; break;
+    case JOB_ASSASSIN:          s =  3; i =  3; d =  6; hp = 15; mp = 0; break;
+    case JOB_STALKER:           s =  2; i =  4; d =  6; hp = 15; mp = 1; break;
 
-    case JOB_HUNTER:            s =  4; i =  3; d =  5; hp = 13; mp = 0; break;
-    case JOB_WARPER:            s =  3; i =  5; d =  4; hp = 12; mp = 1; break;
-    case JOB_ARCANE_MARKSMAN:   s =  3; i =  5; d =  4; hp = 12; mp = 1; break;
+    case JOB_HUNTER:            s =  4; i =  3; d =  5; hp = 16; mp = 0; break;
+    case JOB_WARPER:            s =  3; i =  5; d =  4; hp = 15; mp = 1; break;
+    case JOB_ARCANE_MARKSMAN:   s =  3; i =  5; d =  4; hp = 15; mp = 1; break;
 
-    case JOB_MONK:              s =  3; i =  2; d =  7; hp = 13; mp = 0; break;
-    case JOB_TRANSMUTER:        s =  2; i =  5; d =  5; hp = 12; mp = 1; break;
+    case JOB_MONK:              s =  3; i =  2; d =  7; hp = 16; mp = 0; break;
+    case JOB_TRANSMUTER:        s =  2; i =  5; d =  5; hp = 15; mp = 1; break;
 
-    case JOB_WIZARD:            s = -1; i = 10; d =  3; hp =  8; mp = 5; break;
-    case JOB_CONJURER:          s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_ENCHANTER:         s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_FIRE_ELEMENTALIST: s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_ICE_ELEMENTALIST:  s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_AIR_ELEMENTALIST:  s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_EARTH_ELEMENTALIST:s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_SUMMONER:          s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_VENOM_MAGE:        s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_NECROMANCER:       s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
+    case JOB_WIZARD:            s = -1; i = 10; d =  3; hp = 11; mp = 5; break;
+    case JOB_CONJURER:          s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_ENCHANTER:         s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_FIRE_ELEMENTALIST: s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_ICE_ELEMENTALIST:  s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_AIR_ELEMENTALIST:  s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_EARTH_ELEMENTALIST:s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_SUMMONER:          s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_VENOM_MAGE:        s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_NECROMANCER:       s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
 
     case JOB_WANDERER:
     {
         // Wanderers get their stats randomly distributed.
         _wanderer_assign_remaining_stats(12);
-                                                        hp = 11; mp = 1; break;
+                                                        hp = 14; mp = 1; break;
     }
 
-    case JOB_ARTIFICER:         s =  3; i =  4; d =  5; hp = 13; mp = 1; break;
-    default:                    s =  0; i =  0; d =  0; hp = 10; mp = 0; break;
+    case JOB_ARTIFICER:         s =  3; i =  4; d =  5; hp = 16; mp = 1; break;
+    default:                    s =  0; i =  0; d =  0; hp = 13; mp = 0; break;
     }
 
     you.base_stats[STAT_STR] += s;

dtsund/crawl-light

crawl-ref/source/ng-setup.cc
@@ -149,48 +149,48 @@ static void _jobs_stat_init(job_type which_job)
     // Note: Wanderers are correct, they've got a challenging background. - bwr
     switch (which_job)
     {
-    case JOB_FIGHTER:           s =  8; i =  0; d =  4; hp = 15; mp = 0; break;
-    case JOB_BERSERKER:         s =  9; i = -1; d =  4; hp = 15; mp = 0; break;
-    case JOB_GLADIATOR:         s =  7; i =  0; d =  5; hp = 14; mp = 0; break;
+    case JOB_FIGHTER:           s =  8; i =  0; d =  4; hp = 18; mp = 0; break;
+    case JOB_BERSERKER:         s =  9; i = -1; d =  4; hp = 18; mp = 0; break;
+    case JOB_GLADIATOR:         s =  7; i =  0; d =  5; hp = 17; mp = 0; break;
 
-    case JOB_CRUSADER:          s =  4; i =  4; d =  4; hp = 13; mp = 1; break;
-    case JOB_CHAOS_KNIGHT:      s =  4; i =  4; d =  4; hp = 13; mp = 1; break;
-    case JOB_DEATH_KNIGHT:      s =  5; i =  3; d =  4; hp = 13; mp = 2; break;
-    case JOB_ABYSSAL_KNIGHT:    s =  4; i =  4; d =  4; hp = 13; mp = 1; break;
+    case JOB_CRUSADER:          s =  4; i =  4; d =  4; hp = 16; mp = 1; break;
+    case JOB_CHAOS_KNIGHT:      s =  4; i =  4; d =  4; hp = 16; mp = 1; break;
+    case JOB_DEATH_KNIGHT:      s =  5; i =  3; d =  4; hp = 16; mp = 2; break;
+    case JOB_ABYSSAL_KNIGHT:    s =  4; i =  4; d =  4; hp = 16; mp = 1; break;
 
-    case JOB_HEALER:            s =  5; i =  5; d =  2; hp = 13; mp = 2; break;
-    case JOB_PRIEST:            s =  5; i =  4; d =  3; hp = 13; mp = 2; break;
+    case JOB_HEALER:            s =  5; i =  5; d =  2; hp = 16; mp = 2; break;
+    case JOB_PRIEST:            s =  5; i =  4; d =  3; hp = 16; mp = 2; break;
 
-    case JOB_ASSASSIN:          s =  3; i =  3; d =  6; hp = 12; mp = 0; break;
-    case JOB_STALKER:           s =  2; i =  4; d =  6; hp = 12; mp = 1; break;
+    case JOB_ASSASSIN:          s =  3; i =  3; d =  6; hp = 15; mp = 0; break;
+    case JOB_STALKER:           s =  2; i =  4; d =  6; hp = 15; mp = 1; break;
 
-    case JOB_HUNTER:            s =  4; i =  3; d =  5; hp = 13; mp = 0; break;
-    case JOB_WARPER:            s =  3; i =  5; d =  4; hp = 12; mp = 1; break;
-    case JOB_ARCANE_MARKSMAN:   s =  3; i =  5; d =  4; hp = 12; mp = 1; break;
+    case JOB_HUNTER:            s =  4; i =  3; d =  5; hp = 16; mp = 0; break;
+    case JOB_WARPER:            s =  3; i =  5; d =  4; hp = 15; mp = 1; break;
+    case JOB_ARCANE_MARKSMAN:   s =  3; i =  5; d =  4; hp = 15; mp = 1; break;
 
-    case JOB_MONK:              s =  3; i =  2; d =  7; hp = 13; mp = 0; break;
-    case JOB_TRANSMUTER:        s =  2; i =  5; d =  5; hp = 12; mp = 1; break;
+    case JOB_MONK:              s =  3; i =  2; d =  7; hp = 16; mp = 0; break;
+    case JOB_TRANSMUTER:        s =  2; i =  5; d =  5; hp = 15; mp = 1; break;
 
-    case JOB_WIZARD:            s = -1; i = 10; d =  3; hp =  8; mp = 5; break;
-    case JOB_CONJURER:          s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_ENCHANTER:         s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_FIRE_ELEMENTALIST: s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_ICE_ELEMENTALIST:  s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_AIR_ELEMENTALIST:  s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_EARTH_ELEMENTALIST:s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_SUMMONER:          s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_VENOM_MAGE:        s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
-    case JOB_NECROMANCER:       s =  0; i =  7; d =  5; hp = 10; mp = 3; break;
+    case JOB_WIZARD:            s = -1; i = 10; d =  3; hp = 11; mp = 5; break;
+    case JOB_CONJURER:          s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_ENCHANTER:         s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_FIRE_ELEMENTALIST: s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_ICE_ELEMENTALIST:  s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_AIR_ELEMENTALIST:  s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_EARTH_ELEMENTALIST:s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_SUMMONER:          s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_VENOM_MAGE:        s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
+    case JOB_NECROMANCER:       s =  0; i =  7; d =  5; hp = 13; mp = 3; break;
 
     case JOB_WANDERER:
     {
         // Wanderers get their stats randomly distributed.
         _wanderer_assign_remaining_stats(12);
-                                                        hp = 11; mp = 1; break;
+                                                        hp = 14; mp = 1; break;
     }
 
-    case JOB_ARTIFICER:         s =  3; i =  4; d =  5; hp = 13; mp = 1; break;
-    default:                    s =  0; i =  0; d =  0; hp = 10; mp = 0; break;
+    case JOB_ARTIFICER:         s =  3; i =  4; d =  5; hp = 16; mp = 1; break;
+    default:                    s =  0; i =  0; d =  0; hp = 13; mp = 0; break;
     }
 
     you.base_stats[STAT_STR] += s;