aea28a24b4ca0ecce2a061a46317a9c88624dfc2
Derrick Sund <dtsund@email.arizona.edu>
2012-05-21 16:50:12 -0400

    IMPORT (dolorous): Use a canned message where applicable.

--- crawl-ref/source/item_use.cc
+++ crawl-ref/source/item_use.cc
@@ -1720,7 +1720,7 @@ static bool _blowgun_check(bolt &beam, actor* victim, bool message = true)
         if (victim->atype() == ACT_MONSTER)
             simple_monster_message(victim->as_monster(), " is unaffected.");
         else
-            mpr("You are unaffected.");
+            canned_msg(MSG_YOU_UNAFFECTED);
         return (false);
     }
 
