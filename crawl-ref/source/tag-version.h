77ac8fa34c6262bbe0a4e60beb65b40f8dca18ff
Derrick Sund <dtsund@email.arizona.edu>
2012-05-21 20:10:02 -0400

    Bump up the minor version.

--- crawl-ref/source/tag-version.h
+++ crawl-ref/source/tag-version.h
@@ -9,7 +9,8 @@ enum tag_minor_version
 {
     TAG_MINOR_INVALID         = -1,
     TAG_MINOR_RESET           = 0, // Minor tags were reset
-    TAG_MINOR_DIRECTED_ABYSS,
+    TAG_MINOR_DIRECTED_ABYSS,      // Add the directed Abyss portal.
+    TAG_MINOR_LUA_COLOUR_ENUM,     // Require enums for Lua colours.
 
     NUM_TAG_MINORS,
     TAG_MINOR_VERSION = NUM_TAG_MINORS - 1
