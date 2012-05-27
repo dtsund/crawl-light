bc75096009db91c60c481de747602b7a4eba3968
Derrick Sund <dtsund@email.arizona.edu>
2012-05-27 00:59:48 -0400

    IMPORT (galehar): Fix mons_threat_level leaking info about fake Rakshasa.

--- crawl-ref/source/mon-util.cc
+++ crawl-ref/source/mon-util.cc
@@ -4163,8 +4163,14 @@ bool mons_is_tentacle_end(const int mtype)
 
 int mons_threat_level(const monster *mon)
 {
-    double factor = sqrt(exp_needed(you.experience_level) / 30.0);
-    int tension = exper_value(mon) / (1 + factor);
+    int exp = exper_value(mon);
+
+    // Don't leak info about fake clones.
+    if (!real && mon->props.exists("faking"))
+        exp = exper_value(&mon->props["faking"].get_monster());
+
+    const double factor = sqrt(exp_needed(you.experience_level) / 30.0);
+    const int tension = exp / (1 + factor);
 
     if (tension <= 0)
         // Conjurators use melee to conserve mana, MDFis switch plates...
