08bfe825c1a07f43a714210ed612ce507dc3b9f0
Derrick Sund <dtsund@email.arizona.edu>
2012-08-26 18:47:02 -0700

    IMPORT (kilobyte): An iterator for going through all level_id values.
     It's a travesty something like this is even needed.  Really, all the
     level_type nonsense and portal levels inaccessible except for parsing their
     description should be regular branches.
    
     For now, here's a crutch.

--- crawl-ref/source/travel.h
+++ crawl-ref/source/travel.h
@@ -619,4 +619,14 @@ int click_travel(const coord_def &gc, bool force);
 
 bool check_for_interesting_features();
 
+class level_id_iterator : public std::iterator<std::forward_iterator_tag, level_id>
+{
+public:
+    level_id_iterator();
+    operator bool() const;
+    level_id operator *() const;
+    void operator++();
+private:
+    level_id cur;
+};
 #endif // TRAVEL_H
