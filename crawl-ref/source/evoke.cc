f0cda50fbdc76ba849573fd5057234b421bbc8c0
Derrick Sund <dtsund@email.arizona.edu>
2012-08-28 14:33:09 -0700

    IMPORT (kilobyte): Fix polearms/whips of reaching magically shortening when you're berserk.
     You're not evoking a magic device here, you're just poking someone...

--- crawl-ref/source/evoke.cc
+++ crawl-ref/source/evoke.cc
@@ -651,7 +651,9 @@ static bool _ball_of_energy(void)
 
 bool evoke_item(int slot)
 {
-    if (you.berserk())
+    if (you.berserk() && (slot == -1
+                       || slot != you.equip[EQ_WEAPON]
+                       || !weapon_reach(*you.weapon())))
     {
         canned_msg(MSG_TOO_BERSERK);
         return (false);
