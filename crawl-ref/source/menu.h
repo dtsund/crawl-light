2d4984a00a76d911332425d5acd25c19e322ce26
Derrick Sund <dtsund@email.arizona.edu>
2012-09-01 22:11:42 -0700

    IMPORT (ldierk): new hotkeys and colours for ?/G

--- crawl-ref/source/menu.h
+++ crawl-ref/source/menu.h
@@ -190,6 +190,17 @@ public:
 #endif
 };
 
+class GodMenuEntry : public MenuEntry
+{
+public:
+    GodMenuEntry(const std::string& txt = std::string());
+
+    virtual std::string get_text(const bool unused = false) const;
+private:
+    god_type god;
+    std::string colour_text;
+};
+
 #ifdef USE_TILE
 class PlayerMenuEntry : public MenuEntry
 {
