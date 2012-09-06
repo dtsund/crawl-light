57b14eb7ef7bd816d7fb44c2c4357b8af167aa3b
Derrick Sund <dtsund@email.arizona.edu>
2012-09-05 17:48:55 -0700

    IMPORT (SamB): Start work on a generic god menu.
     So far, this consists of moving GodMenuEntry to a new module and slightly
     altering its interface to be more useful outside of ?/G.

--- crawl-ref/source/command.cc
+++ crawl-ref/source/command.cc
@@ -26,6 +26,7 @@
 #include "describe.h"
 #include "files.h"
 #include "ghost.h"
+#include "godmenu.h"
 #include "invent.h"
 #include "itemname.h"
 #include "items.h"
@@ -1807,7 +1808,7 @@ static void _find_description(bool *again, std::string *error_inout)
         else if (doing_features)
             me = new FeatureMenuEntry(str, feat_by_desc(str), letter);
         else if (doing_gods)
-            me = new GodMenuEntry(key_list[i]);
+            me = new GodMenuEntry(str_to_god(key_list[i]));
         else
         {
             me = new MenuEntry(uppercase_first(key_list[i]), MEL_ITEM, 1,
