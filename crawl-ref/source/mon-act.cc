a208434a8b7bcfb6c92edf74339fe58833f1ec33
Derrick Sund <dtsund@email.arizona.edu>
2013-09-04 01:20:28 -0700

    Remove an inaccurate comment; the code actually DOES check for poison and so forth.

--- crawl-ref/source/mon-act.cc
+++ crawl-ref/source/mon-act.cc
@@ -1982,7 +1982,6 @@ static bool _handle_throw(monster* mons, bolt & beem, bool sidestep_attempt)
 
     bool should_be_punished = false;
     // Try not to piss off Zin.
-    // NOTE: DOES NOT YET CHECK SICKEN/POISON/WEIRD STUFF LIKE THAT
     if (beem->has_illegal_ranged_brand() || is_edict_active(EDICT_NO_PROJECTILES))
     {
         if (!mons->should_break_edict())
