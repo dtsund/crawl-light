crawl/crawl

Windows (MinGW): Do crash dumps, same as on *nix.
We do this by defining USE_UNIX_SIGNALS there and fixing the resulting
compiler errors.

For some reason they aren't being written to files yet, though :-(.

crawl-ref/source/main.cc
@@ -2035,7 +2035,7 @@ void process_command(command_type cmd)
         // because we want to have CTRL-Y available...
         // and unfortunately they tend to be stuck together.
         clrscr();
-#ifndef USE_TILE_LOCAL
+#if !defined(USE_TILE_LOCAL) && !defined(TARGET_OS_WINDOWS)
         console_shutdown();
         kill(0, SIGTSTP);
         console_startup();

dtsund/crawl-light

Conflicts:
	crawl-ref/source/AppHdr.h
	crawl-ref/source/crash-u.cc
	crawl-ref/source/main.cc

crawl-ref/source/main.cc
@@ -2079,7 +2079,7 @@ void process_command(command_type cmd)
         // because we want to have CTRL-Y available...
         // and unfortunately they tend to be stuck together.
         clrscr();
-#ifndef USE_TILE
+#if !defined(USE_TILE) && !defined(TARGET_OS_WINDOWS)
         unixcurses_shutdown();
         kill(0, SIGTSTP);
         unixcurses_startup();